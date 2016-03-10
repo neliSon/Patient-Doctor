cb22ccaab64a9cea7d7ae59431825a544f665df3
neliSun <nelson.chow91.nc@gmail.com>
2016-03-10 11:06:57 -0800

    Initial Commit

---
+++ Patient&Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient&Doctor
+//
+//  Created by Nelson Chow on 2016-03-10.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
